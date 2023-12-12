b28ab4a61b0b883120c672fd3bf57a6abd33c962
FransergioDev <fransergio.dev@gmail.com>
2023-12-12 19:50:56 -0300

    Adjust responsive layout gym space images

--- src/components/our-space/index.tsx
+++ src/components/our-space/index.tsx
@@ -10,25 +10,25 @@ export default function OurSpace() {
         <section className='text-left relative h-[100%]' id="our-space">
             <h2 className='font-extrabold text-4xl mb-4'>Nosso espaço</h2>
             <div className='grid grid-row gap-x-8 gap-y-[2em] bg-yellow z-0 rounded-md relative content-between p-2 pt-8'>
-                <div className='bg-white w-[20%] left-0 h-[100%] z-1 absolute'></div>
-                <div className='bg-black w-[75%] left-0 mt-[2.5em] h-[95%] z-2 absolute rounded-md'></div>
-                <div className="grid grid-cols-2 z-[88888] mt-8">
-                    <img src={image1} alt='' className='col-span-1 max-h-[500px] max-w-[800px] w-[100%]'/>
+                <div className='bg-white w-[20%] left-0 h-[100%] z-1 absolute max-[715px]:pt-2 max-[715px]:pb-2'></div>
+                <div className='bg-black w-[75%] left-0 mt-[2.5em] h-[95%] z-2 absolute rounded-md max-[715px]:h-[100%] max-[715px]:mt-[0em] max-[715px]:pt-2 max-[715px]:pb-2'></div>
+                <div className="grid grid-cols-2 z-[88888] mt-8 max-[715px]:mt-0">
+                    <img src={image1} alt='' className='col-span-1 max-h-[500px] max-w-[800px] w-[100%] max-[715px]:col-span-2'/>
                 </div>
                 <div className="grid grid-cols-2 z-[88888]">
-                    <img src={image2} alt='' className='col-start-2 col-span-1 max-h-[500px] max-w-[800px] w-[100%]'/>
+                    <img src={image2} alt='' className='col-start-2 col-span-1 max-h-[500px] max-w-[800px] w-[100%] max-[715px]:col-span-2'/>
                 </div>
                 <div className="grid grid-cols-2 z-[88888]">
-                    <img src={image3} alt='' className='col-span-1 max-h-[500px] max-w-[800px] w-[100%]'/>
+                    <img src={image3} alt='' className='col-span-1 max-h-[500px] max-w-[800px] w-[100%] max-[715px]:col-span-2'/>
                 </div>
                 <div className="grid grid-cols-2 z-[88888]">
-                    <img src={image4} alt='' className='col-start-2 col-span-1 max-h-[500px] max-w-[800px] w-[100%]'/>
+                    <img src={image4} alt='' className='col-start-2 col-span-1 max-h-[500px] max-w-[800px] w-[100%] max-[715px]:col-span-2'/>
                 </div>
                 <div className="grid grid-cols-2 z-[88888]">
-                    <img src={image5} alt='' className='col-span-1 max-h-[500px] max-w-[800px] w-[100%]'/>
+                    <img src={image5} alt='' className='col-span-1 max-h-[500px] max-w-[800px] w-[100%] max-[715px]:col-span-2'/>
                 </div>
                 <div className="grid grid-cols-2 z-[88888]">
-                    <img src={image6} alt='' className='col-start-2 col-span-1 max-h-[500px] max-w-[800px] w-[100%]'/>
+                    <img src={image6} alt='' className='col-start-2 col-span-1 max-h-[500px] max-w-[800px] w-[100%] max-[715px]:col-span-2'/>
                 </div>
             </div>
         </section>
